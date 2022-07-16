145597536b50982852212e150692eb7655293f3a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-16 15:18:44 +0530

    Create cartapi.service.ts

---
+++ src/app/services/cartapi.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartapiService {
+  cartDataList:any =[];
+  productList= new BehaviorSubject<any>([]);
+  constructor(private http:HttpClient) { }
+  //Get Product Data
+  getProductData(){
+    return this.productList.asObservable();
+  }
+  //Set Product Data
+  setProduct(product:any){
+    this.cartDataList.push(...product);
+    this.productList.next(product)
+  }
+  //Add to cart details
+  addToCart(product:any) {
+    this.cartDataList.push(product);
+    this.productList.next(this.cartDataList)
+    this.getTotalAmount();
+    console.log(this.cartDataList)
+  }
+  //Get Total amount
+  getTotalAmount():number {
+    let grandTotal=0;
+    this.cartDataList.map((a:any)=>{
+      grandTotal += parseFloat(a.price);
+    })
+    return grandTotal;
+  }
+  //Remove Cart data one by one
+  removeCardData(product:any) {
+    this.cartDataList.map((a:any, index:any)=>{
+      if(product.id === a.id){
+        this.cartDataList.splice(index, 1)
+      }
+    })
+    this.productList.next(this.cartDataList)
+  }
+  //Remove All Cart Data
+  removeAllCart() {
+    this.cartDataList =[]
+    this.productList.next(this.cartDataList)
+  }
+}
